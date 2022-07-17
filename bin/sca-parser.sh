abe9d81dee615a6ee10a5de295c1050806b93bd9
Ann Davis <andavis@suse.com>
2022-07-17 15:42:26 -0600

    dev

--- bin/sca-parser.sh
+++ bin/sca-parser.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 #
-# This script parses results from various sca tools then uses tool config settings
-# to provide next-step guidance.
+# This script parses results from one or more sca tool result files. If only one tool
+# results file is provided, the parser takes the actions as defined in the tool result
+# file.  If multiple tool results files are provided, the parser takes actions as defined
+# in the parser config file.
 #
-# Inputs: SCA tool results file
-#         (optional with -o) output file listing guidance (next steps)
+# Inputs: SCA tool results file(s)
 #
-# Output: List of next steps
+# Outputs/Results: Actions as described in tool and/or parser config files
 #
 
 #
@@ -17,15 +18,14 @@ function usage() {
 	echo "Usage: `basename $0` [options] <sca-tool-results-file>"
 	echo "Options:"
 	echo "    -d        debug"
-	echo "    -o        output file"
+	echo "    -o	    output file"
 }
 
 function exitError() {
-	echo "$1"
-	rm -rf $tmpDir 2>/dev/null
-	exit 1
+        echo "$1"
+        exit 1
 }
-
+ 
 #
 # main
 #
@@ -57,75 +57,87 @@ while getopts 'hdo:' OPTION; do
         esac
 done
 shift $((OPTIND - 1))
-if [ ! "$1"} ]; then
+if [ ! "$1" ]; then
         usage
         exit 1
 else
-	toolResultsFile="$1"
-	toolResultsPath=`dirname $toolResultsFile`
-fi
-[ $DEBUG ] && echo "*** DEBUG: $0: toolResultsFile: $toolResultsFile" >&2
-
-# sca-parser conf file
-curPath=`dirname "$(realpath "$0")"`
-parserConfFile="/etc/sca-parser.conf"
-if [ ! -r "$parserConfFile" ]; then
-        parserConfFile="/usr/etc/sca-parser.conf"
-        if [ ! -r "$parserConfFile" ]; then
-               	parserConfFile="$curPath/../sca-parser.conf"
-               	if [ ! -r "$parserConfFile" ]; then
-			# do something else?
-                       	exitError "No sca-parser conf file info; exiting..."
+	declare -a toolResultsFiles="$@"
+	for toolResultsFile in $toolResultsFiles; do
+		if [ ! -r "$toolResultsFile" ]; then
+			echo "Tool results file $toolResultsFile unreadable, exiting..."
+			exit 1
 		fi
-        fi
+	done
 fi
-[ $DEBUG ] && echo "*** DEBUG: $0: parserConfFile: $parserConfFile" >&2
-source $parserConfFile
-parserBinPath="$SCA_PARSER_BIN_PATH"
-parserVarPath="$SCA_PARSER_VAR_PATH"
-[ $DEBUG ] && echo "*** DEBUG: $0: parserBinPath: $parserBinPath, parserVarPath: $parserVarPath" >&2
+[ $DEBUG ] && echo "*** DEBUG: $0: toolResultsFiles: $toolResultsFiles" >&2
 
-# get the prio group categories by looking for *-p?-categories in tool results file
-prioGroups=`grep -E "p?-categories:" $toolResultsFile | cut -d':' -f1 | rev | cut -d'-' -f2 | rev | tr '\n' ' '`
-[ $DEBUG ] && echo "*** DEBUG: $0: prioGroups: $prioGroups" >&2
+curPath=`dirname "$(realpath "$0")"`
+#parentPid=$PPID
+#parent=`ps -o cmd= -q $PPID`
+#[ $DEBUG ] && echo "*** DEBUG: $0: parent: $parent" >&2
+
+# conf file 
+parserConfFiles="/usr/etc/sca-parser.conf /etc/sca-parser.conf ${curPath}/../sca-parser.conf"
+for parserConfFile in ${parserConfFiles}; do
+	if [ -r "${parserConfFile}" ]; then
+		found="true"
+		[ $DEBUG ] && echo "*** DEBUG: $0: reading ${parserConfFile}" >&2
+		. ${parserConfFile}
+	fi
+done
+if [ ! "${found}" ]; then
+	exitError "No parser conf file info; exiting..."
+fi
+parserVarPath="$SCA_PARSER_VAR_PATH"
+toolPrio="$SCA_PARSER_TOOL_PRIO"
+[ $DEBUG ] && echo "*** DEBUG: $0: parserVarPath: $parserVarPath" >&2
 
 # Take actions
 actionsTaken=""
 ts=`date +%s`
-for prioGroup in $prioGroups; do
-        [ $DEBUG ] && echo "*** DEBUG: $0: prioGroup: $prioGroup" >&2
-        actions=`grep "${prioGroup}-actions:" $toolResultsFile | cut -d":" -f1 --complement`
-        [ $DEBUG ] && echo "*** DEBUG: $0: actions: $actions" >&2
-        for action in $actions; do
-                actionName=`echo $action | cut -d':' -f1`
-                actionDeps=`echo $action | cut -d':' -f2`
-                [ $DEBUG ] && echo "*** DEBUG: $0: actionName: $actionName, actionDeps: $actionDeps" >&2
-                if echo "$actionsTaken" | grep -q "$actionName"; then
-                        [ $DEBUG ] && echo "*** DEBUG: $0: action $actionName already taken" >&2
-                        continue
-                fi
-                if [ "$actionDeps" = "*" ]; then
-                        [ $DEBUG ] && echo "***  DEBUG: $0: taking action $actionName (no dependency on result)" >&2
-                        mkdir -p "${actionsVarPath}/${actionName}" 2>/dev/null
-                        touch "${actionsVarPath}/${actionName}/test.$ts"
-                        actionsTaken="$actionsTaken $actionName"
-                        continue
-                fi
-                categories=`grep "${prioGroup}-categories:" $toolResultsFile | cut -d":" -f2`
-                [ $DEBUG ] && echo "*** DEBUG: $0: categories: $categories" >&2
-                for category in $categories; do
-                        [ $DEBUG ] && echo "*** DEBUG: $0: category: $category" >&2
-                        categoryResult=`grep "${category}-result" $toolResultsFile | cut -d':' -f2 | sed "s/^ *//"`
-                        [ $DEBUG ] && echo "*** DEBUG: $0: categoryResult: $categoryResult" >&2
-                        if echo "$actionDeps" | grep -qE "^$categoryResult|,$categoryResult"; then
-                                [ $DEBUG ] && echo "*** DEBUG: $0: taking action $actionName based on category result" >&2
-                                mkdir -p "$parserVarPath}/${actionName}" 2>/dev/null
-                                touch "${parserVarPath}/${actionName}/test.$ts"
-                                actionsTaken="$actionsTaken $actionName"
-                                break
-                        fi
-                done
-        done
-done
+numToolFiles=`echo $toolResultsFiles | wc -w`
+if [ $numToolFiles = 1 ]; then
+	[ $DEBUG ] && echo "*** DEBUG: $0: toolResultsFile: $toolResultsFile" >&2
+	prioGroups=`grep -E "p?-categories:" $toolResultsFile | cut -d':' -f1 | rev | cut -d'-' -f2 | rev | tr '\n' ' '`
+	[ $DEBUG ] && echo "*** DEBUG: $0: prioGroups: $prioGroups" >&2
+	for prioGroup in $prioGroups; do
+        	[ $DEBUG ] && echo "*** DEBUG: $0: prioGroup: $prioGroup" >&2
+        	actions=`grep "${prioGroup}-actions:" $toolResultsFile | cut -d":" -f1 --complement`
+        	[ $DEBUG ] && echo "*** DEBUG: $0: actions: $actions" >&2
+        	for action in $actions; do
+                	actionName=`echo $action | cut -d':' -f1`
+                	actionDeps=`echo $action | cut -d':' -f2`
+                	[ $DEBUG ] && echo "*** DEBUG: $0: actionName: $actionName, actionDeps: $actionDeps" >&2
+                	if echo "$actionsTaken" | grep -q "$actionName"; then
+                        	[ $DEBUG ] && echo "*** DEBUG: $0: action $actionName already taken" >&2
+                        	continue
+                	fi
+                	if [ "$actionDeps" = "*" ]; then
+                        	[ $DEBUG ] && echo "***  DEBUG: $0: taking action $actionName (no dependency on result)" >&2
+                        	mkdir -p "${actionsVarPath}/${actionName}" 2>/dev/null
+                        	touch "${actionsVarPath}/${actionName}/test.$ts"
+                        	actionsTaken="$actionsTaken $actionName"
+                        	continue
+                	fi
+                	categories=`grep "${prioGroup}-categories:" $toolResultsFile | cut -d":" -f2`
+                	[ $DEBUG ] && echo "*** DEBUG: $0: categories: $categories" >&2
+                	for category in $categories; do
+                        	[ $DEBUG ] && echo "*** DEBUG: $0: category: $category" >&2
+                        	categoryResult=`grep "${category}-result" $toolResultsFile | cut -d':' -f2 | sed "s/^ *//"`
+                        	[ $DEBUG ] && echo "*** DEBUG: $0: categoryResult: $categoryResult" >&2
+                        	if echo "$actionDeps" | grep -qE "^$categoryResult|,$categoryResult"; then
+                                	[ $DEBUG ] && echo "*** DEBUG: $0: taking action $actionName based on category result" >&2
+                                	mkdir -p "$parserVarPath}/${actionName}" 2>/dev/null
+                                	touch "${parserVarPath}/${actionName}/test.$ts"
+                                	actionsTaken="$actionsTaken $actionName"
+                                	break
+                        	fi
+                	done
+        	done
+	done
+	echo "actionsTaken: $actionsTaken" >> $outFile
+else
+	# what to do when taksi calls the parser?
+fi
 
 exit 0
